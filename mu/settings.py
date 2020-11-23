ab9b205db8eeb463540510e666c6a58ebee091fd
Philippe Martinet <phi.mar.email@gmail.com>
2020-11-23 12:41:00 +0100

    second commit

--- mu/settings.py
+++ mu/settings.py
@@ -120,3 +120,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 ENCRYPT_KEY = b'nLSOyfzWTyEPvkkkKIK5n1jXc2N1IwkV0RvfMTXWbC0='
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
