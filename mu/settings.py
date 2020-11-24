91b21d696be2f4530357877bcd588074f17dd55d
Philippe Martinet <phi.mar.email@gmail.com>
2020-11-24 21:20:48 +0100

    Change sqlite3 -> postgres

--- mu/settings.py
+++ mu/settings.py
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'mu.wsgi.application'
 
 DATABASES = {}
 db_from_env = dj_database_url.config()
-#DATABASES['default'].update(db_from_env)
-DATABASES['default']=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+#DATABASES['default']=dj_database_url.config(conn_max_age=600)
 
 # DATABASES={    'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
